47d467c4ccf8252c1001a1f32824b4393b5e4316
Kate Lueders <kelueders@gmail.com>
2023-11-09 07:03:24 -0600

    first commit

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
+    """
+        Set Config variables for the flask app.
+    """
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off messages for updates in sqlalchemy
