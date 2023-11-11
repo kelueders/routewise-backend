f7024eaaa6b4ad0bf069fb3ebdf36af8c55e662c
Kate Lueders <kelueders@gmail.com>
2023-11-11 08:58:19 -0600

    build: add CORS, psycop, gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from config import Config
 from flask_migrate import Migrate
-
+from flask_cors import CORS
 
 # INTERNAL
 from .profile.routes import profile
@@ -10,12 +10,14 @@ from .profile.models import db
 
 app = Flask(__name__)
 
+# Blueprints for each user story
 app.register_blueprint(profile)
 
 app.config.from_object(Config)
 
 db.init_app(app)
 migrate = Migrate(app, db)        # the instance for the database migration engine
+CORS(app)                         # what is this for?????
 
 # Test route
 @app.route('/kate')
--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class Config():
     """
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off messages for updates in sqlalchemy
