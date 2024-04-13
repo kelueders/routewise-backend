27cbdee8a2ad33f8c19e1acac63e4ea81df6e4cd
Kate Lueders <kelueders@gmail.com>
2024-04-12 21:43:07 -0500

    refactor: clean up print statements, etc

--- app/itinerary/helpers.py
+++ app/itinerary/helpers.py
@@ -14,9 +14,6 @@ places_copy - dict type
 '''
 def create_itinerary(places, duration):
 
-    print("PLACES")
-    print(places)
-
     days = {}
     day_order = []    # [day-1, day-2, day-3, ...]
     for i in range(duration):
@@ -25,14 +22,6 @@ def create_itinerary(places, duration):
             'placeIds': []
         } 
         day_order.append(f'day-{i + 1}') 
-
-    # places_dict = {}   # creates a serialized list (dict) that holds the place id as a key for each place object
-
-    # for place in places.keys():
-    #     places_dict[place] = place
-
-    # print("PLACES_DICT")
-    # print(places_dict)
 
     # if only one place given - create days dict and day_order list, then return simply itinerary
     if len(places) < 2:
@@ -43,11 +32,8 @@ def create_itinerary(places, duration):
             "days": days,
             "day_order": day_order
         }
-    
 
-
-    # create a list just containing the coordinates to be used to create the matrix of
-    #      all the distances
+    # create a list just containing the coordinates to be used to create the matrix of all the distances
     coords = []
     for place in places.values():
 
@@ -62,13 +48,8 @@ def create_itinerary(places, duration):
     dist_range = 0
 
     places_list = list(places_copy.values())
-    
-    # print(type(places_list))
-    print("PLACES LIST")
-    # print(places_list)
 
-    # creates a key within the places_copy dict that holds a dict containing the distances
-    #       to each of the other locations
+    # creates a key within the places_copy dict that holds a dict containing the distances to each of the other locations
     i = 0
     for place in places_list:
 
@@ -86,21 +67,10 @@ def create_itinerary(places, duration):
         place['sum_dist'] = sum(all_dist[i])
         i += 1
 
-    # print(all_dist)
-    # print(dist_range)
-    
+    # The chosen range is 15% when comparing locations to eachother
     threshold_range = 0.15 * dist_range
 
-    ''' 1. Finds the location amongst the list of places that is furthest from the others.
-        2. Removes that place from the places_copy list.
-        3. Appends that location to the day_places list (see below)
-        4. Continues this process X times where X is the number of days of the trip'''
-    c = 0
-    day_places = []      # gives you a list of place ids that matches the number of days, with each place being the furthest from others in order to create zones the user will visit each day
-    co_captains = {}
-
-    for day_num in day_order:
-        
+    for day_num in day_order:        
 
         max_distance = 0
         max_place_id = ''
@@ -108,33 +78,26 @@ def create_itinerary(places, duration):
         # step 1
         for place in places_list:
 
-            # print(place)
-
             if max_distance < place['sum_dist']: 
                 max_distance = place['sum_dist']
                 max_place_id = place['local_id']  
 
 
-        if max_place_id: # there won't be an id if there are no more places left in places_copy
+        if max_place_id: # there won't be an id if there are no more places left in places_list
 
             # step 1b - add captain's place id to the current day 
             captain_id = max_place_id
             days[day_num]['placeIds'] = [captain_id]                                   # WHY IS THIS IN BRACKETS?
 
-            # print(captain_id)
             captain_dict = list(filter(lambda x : True if x['local_id'] == captain_id else False, places_list ))
 
             # step 1c - remove captain place object from places_list
             place_index = places_list.index(captain_dict[0])
             places_list.pop(place_index)
 
-            print(places_list)
-
             # step 2 - find co-captains of the selected captain (places that are within 15% range distance of the selected captain place)
             sorted_places_copy = sorted(places_list, key=lambda x: x['place_distances'][captain_id]) # sorted by distance 
 
-            print("SORTED")
-            print(sorted_places_copy)
 
             for place in sorted_places_copy:
 
@@ -143,20 +106,15 @@ def create_itinerary(places, duration):
                     if place['place_distances'][captain_id] < threshold_range:
                         days[day_num]['placeIds'].append(place['local_id'])
 
-                        print("LOCAL ID")
-                        print(place['local_id'])
-
                         places_list = list(filter(lambda x : True if x['local_id'] != place['local_id'] else False, places_list ))
-                        # place_index = places_list.index(place['local_id'])
-                        # places_list.pop(place_index)
 
 
-    # step 3 - add remaining places to day with closest day captain 
-    # create places_copy remove list
-    remove_list = [] # place ids to remove from places_copy after finishing the loop
+    # create places_list remove list
+    remove_list = [] # place ids to remove from places_list after finishing the loop
 
-    # loop thru the remaining places in places_copy
-    for place in places_copy.values():
+    # loop thru the remaining places in places_list
+    # for place in places_copy.values():
+    for place in places_list:
 
         # create closest_captain_id and closest_day_num "day-2"
         closest_captain_id = ""
@@ -187,20 +145,10 @@ def create_itinerary(places, duration):
 
             days[closest_day_num]['placeIds'].append(place['local_id'])
 
-            # add the place id to a places_copy remove list
+            # add the place id to a places_list remove list
             remove_list.append(place['local_id'])
 
     saved_places = list(filter(lambda x : True if x['local_id'] not in remove_list else False, places_list))
-    # remove the 'remove_list' places from the places_copy
-    # for place_id in remove_list:
-
-    #     # place_index = places_list.index(places_dict[place_id])
-    #     places_copy.pop(place_id)
-
-    # remaining places in places_copy will be added to a 'saved_places' list [{place 1}, {place 2} ... ]
-    # saved_places = []
-    # for place in places_copy.values():
-    #     saved_places.append(place['local_id'])
 
     return {
         "days": days,
