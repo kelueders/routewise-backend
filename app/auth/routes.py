dde8dc3bcc69fcd73c94762a53702a98e5daad8a
Kate Lueders <kelueders@gmail.com>
2024-06-24 21:35:12 -0500

    hide passcode

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request, redirect, url_for
 
 # INTERNAL
 from app.models import User, db
+from app.hidden import entry_code
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -10,8 +11,6 @@ auth = Blueprint('auth', __name__, url_prefix='/auth')
 # If it's the same, then will change user has_access attribute to True
 @auth.route('/check_code/', methods = ['GET', 'PATCH'])
 def check_code():
-
-    entry_code = "124453"
 
     uid = request.json['uid']
     passcode = request.json['passcode']
@@ -24,15 +23,5 @@ def check_code():
         return "Access granted"
     else:
         return "Access NOT granted"
-
-# NOT NEEDED, will be checking the Firebase database to see if user has access
-# Checks if the user has the value 'True' for their has_access attribute
-# @auth.route('/check_auth/<uid>', methods=['GET'])
-# def check_auth(uid):
-    
-#     user = User.query.filter_by(uid = uid).first()
-
-#     if user.has_access == True:
-#         return redirect(url_for(''))
 
 
