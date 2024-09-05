59e0a0bad38f3acf22f0b37468c4875de8d501f6
kaitlynnchan <kaitlynnchan@outlook.com>
2024-09-04 23:02:12 -0700

    Fix adding places bug
    - added in_itinerary to places
    - renamed placeId parameter

--- app/global_helpers.py
+++ app/global_helpers.py
@@ -109,44 +109,30 @@ def add_places(trip_id, places_last, places_arr):
 
         if type(places_arr) == dict:
             place = places_arr[i + 1]
-            
             local_id = place['local_id']
-            place_name = place['placeName']
-            geoapify_placeId = place['placeId']
-            place_address = place['address']
-            place_img = place['imgURL']
-            favorite = place['favorite']            
-            category = place.get('category', None)
-            phone_number = place.get('phoneNumber', None)
-            rating = place.get('rating', None)
-            summary = place.get('summary', None)
-            website = place.get('website', None)
-            avg_visit_time = place.get('avgVisitTime', 60)
-            info = place['info']
-            lat = place['lat']
-            long = place['long']   
-
         elif type(places_arr) == list:
             place = places_arr[i]
-
             local_id = place['id']
-            place_name = place['placeName']
-            geoapify_placeId = place['place_id']
-            place_address = place['address']
-            place_img = place['imgURL']
-            category = place.get('category', None)
-            phone_number = place.get('phoneNumber', None)
-            rating = place.get('rating', None)
-            summary = place.get('summary', None)
-            website = place.get('website', None)
-            avg_visit_time = place.get('avgVisitTime', 60)
-            favorite = place['favorite']
-            info = place['info']
-            lat = place['lat']
-            long = place['long']        
+
+        place_name = place['placeName']
+        geoapify_placeId = place['placeId']
+        place_address = place['address']
+        place_img = place['imgURL']
+        favorite = place['favorite']
+        category = place.get('category', None)
+        phone_number = place.get('phoneNumber', None)
+        rating = place.get('rating', None)
+        summary = place.get('summary', None)
+        website = place.get('website', None)
+        avg_visit_time = place.get('avgVisitTime', 60)
+        info = place['info']
+        lat = place['lat']
+        long = place['long']
+        in_itinerary = False
 
         place = Place(local_id, place_name, geoapify_placeId, place_address, place_img, 
-                      info, favorite, category, phone_number, rating, summary, website, avg_visit_time, lat, long, trip_id)
+                      info, favorite, category, phone_number, rating, summary, website, 
+                      avg_visit_time, lat, long, in_itinerary, trip_id)
 
         db.session.add(place)
         db.session.commit()
