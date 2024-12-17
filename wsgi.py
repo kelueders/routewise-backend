1b6451b805851edf296590614ec36a48df7cfb5b
kaitlynnchan <kaitlynnchan@outlook.com>
2024-12-16 16:44:21 -0800

    Added comments for new files

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
 from app import app
 
+'''
+Web Server Gateway Interface
+
+This file acts as en entry point for deploying the flask app in a 
+production environment. The environment will run on Gunicorn.
+
+'''
 if __name__ == '__main__':
     app.run()
